b4d687cdc1d5f2787760c076082f294e52f97b6e
cmlfxz <luoguoqi.ang@163.com>
2020-08-15 20:17:56 +0800

    add post

--- dev-jenkinsfile.java
+++ dev-jenkinsfile.java
@@ -120,6 +120,7 @@ pipeline {
                 robot: '4def1f0b-4f7c-4793-b1d0-6f5394afa257',
                 type: 'LINK',
                 messageUrl:'https://oapi.dingtalk.com/robot/send?access_token=bba613c1e866e921d3075c21c8eda6aac020d6a7f679974645ddd05cb33a59e8', 
+                text:["更新记录"],
                 picUrl:'', 
                 // jenkinsUrl:'http://http://192.168.11.142:8080/jenkins/', 
                 title:'构建成功', 
