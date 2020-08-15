1465a4004f816e6f5543b5c4ea6362a0f0a00cfc
cmlfxz <luoguoqi.ang@163.com>
2020-08-15 21:29:42 +0800

    add post

--- dev-jenkinsfile.java
+++ dev-jenkinsfile.java
@@ -104,10 +104,10 @@ pipeline {
                     messageUrl:'https://oapi.dingtalk.com/robot/send?access_token=bba613c1e866e921d3075c21c8eda6aac020d6a7f679974645ddd05cb33a59e8', 
                     title:'$PROJECT >> dev >> ${SERVICE} 更新成功', 
                     text:[
-                        "- 任务: #${BUILD_ID}",
+                        "- 任务: $BUILD_DISPLAY_NAME",
                         "- 状态: 成功",
                         "- 持续时间: ${currentBuild.durationString}",
-                        "- 执行人：${BUILD_USER}",
+                        "- 执行人：${CHANGE_AUTHOR}",
                     
                     ],
                     picUrl:'http://kmzsccfile.kmzscc.com/upload/2020/success.jpg',
