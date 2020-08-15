9994618a290198c8f4ceebd4f8720910b8904448
cmlfxz <luoguoqi.ang@163.com>
2020-08-15 21:42:44 +0800

    mod jenkinsfile

--- dev-jenkinsfile.java
+++ dev-jenkinsfile.java
@@ -104,8 +104,10 @@ pipeline {
                         '18688376362'
                     ],
                     messageUrl:'https://oapi.dingtalk.com/robot/send?access_token=bba613c1e866e921d3075c21c8eda6aac020d6a7f679974645ddd05cb33a59e8', 
-                    title:'$PROJECT >> dev >> ${SERVICE} 更新成功', 
+                    // title:'$PROJECT >> dev >> ${SERVICE} 更新成功', 
                     text:[
+                        "$PROJECT >> dev >> ${SERVICE} 更新成功",
+                        "------------------------------------",
                         "- 任务: $BUILD_DISPLAY_NAME",
                         "- 状态: 成功",
                         "- 持续时间: ${currentBuild.durationString}",
