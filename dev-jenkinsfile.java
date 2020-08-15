62de173e5d8f21c1d01becaa54a730d7f6665218
cmlfxz <luoguoqi.ang@163.com>
2020-08-15 16:34:47 +0800

    add post

--- dev-jenkinsfile.java
+++ dev-jenkinsfile.java
@@ -94,4 +94,25 @@ pipeline {
 
     }
 
+    post {
+        success {
+            emailext (
+                subject: "SUCCESSFUL: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
+                body: """<p>SUCCESSFUL: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
+                    <p>Check console output at "<a href="${env.BUILD_URL}">${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>"</p>""",
+                to: "915613275@qq.com,18688376362@163.com",
+                from: "luoguoqi.ang@163.com"
+            )
+        }
+        failure {
+            emailext (
+                subject: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
+                body: """<p>FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
+                    <p>Check console output at "<a href="${env.BUILD_URL}">${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>"</p>""",
+                to: "915613275@qq.com,18688376362@163.com",
+                from: "luoguoqi.ang@163.com"
+            )
+        }
+    }
+
 }
